bff6339c9cb769b6b9da8ff47f399ec25419f69a
himan7991 <himan7991@gmail.com>
2024-01-02 19:45:13 +0200

    ci(Page1): add content

--- src/pages/Page1.tsx
+++ src/pages/Page1.tsx
@@ -1,17 +1,25 @@
-export default function Page1() {
+import { motion } from 'framer-motion'
+
+export default function Page1({ getStarted }: { getStarted: () => void }) {
 	return (
-		<div>
+		<motion.div id="page1" layoutId="pages">
 			<div className="text-center">
 				<h1 className="text-4xl font-bold tracking-tight text-copy sm:text-6xl">Vacation Time Optimizer</h1>
 				<p className="mt-6 text-lg leading-8 text-copy-light">
 					Optimize your time, maximize your escape: find the perfect moment for your getaway
 				</p>
 				<div className="mt-10 flex items-center justify-center gap-x-6">
-					<button className="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">
+					<button
+						onClick={() => getStarted()}
+						className="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
+					>
 						Get started
 					</button>
+					{/* <a href="#" className="text-sm font-semibold leading-6 text-copy">
+							Learn more <span aria-hidden="true">→</span>
+						</a> */}
 				</div>
 			</div>
-		</div>
+		</motion.div>
 	)
 }
