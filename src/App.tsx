9d8e5d7996a246c95bb62653cffc5cccc8808c1f
himan7991 <himan7991@gmail.com>
2024-01-02 18:37:25 +0200

    style: add base colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import styles from './styles/App.module.css'
+import { useEffect } from 'react'
 
-function App() {
-	return <></>
+export default function App() {
+	useEffect(() => {
+		const selectedTheme = localStorage.getItem('theme')
+
+		if (selectedTheme) {
+			// if the user has selected a theme, use that
+			document.body.classList.add(selectedTheme)
+
+			// else if the user's OS prefers dark mode, use that
+		} else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			document.body.classList.add('dark')
+
+			// else use light mode
+		} else {
+			document.body.classList.add('light')
+		}
+	}, [])
+
+	return <main className="min-h-screen bg-background"></main>
 }
-
-export default App
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,35 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "hsl(var(--primary))",
+        "primary-content": "hsl(var(--primary-content))",
+        "primary-dark": "hsl(var(--primary-dark))",
+        "primary-light": "hsl(var(--primary-light))",
+
+        secondary: "hsl(var(--secondary))",
+        "secondary-content": "hsl(var(--secondary-content))",
+        "secondary-dark": "hsl(var(--secondary-dark))",
+        "secondary-light": "hsl(var(--secondary-light))",
+
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        border: "hsl(var(--border))",
+
+        copy: "hsl(var(--copy))",
+        "copy-light": "hsl(var(--copy-light))",
+        "copy-lighter": "hsl(var(--copy-lighter))",
+
+        success: "hsl(var(--success))",
+        warning: "hsl(var(--warning))",
+        error: "hsl(var(--error))",
+
+        "success-content": "hsl(var(--success-content))",
+        "warning-content": "hsl(var(--warning-content))",
+        "error-content": "hsl(var(--error-content))",
+      },
+    }
   },
   plugins: [],
 }
