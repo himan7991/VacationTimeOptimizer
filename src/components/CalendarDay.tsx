75235bc73f29c4f643b60f08c41af4e77833c368
himan7991 <himan7991@gmail.com>
2024-01-03 23:28:19 +0200

    refactor(CalendarDay): make it much more concise

--- src/components/CalendarDay.tsx
+++ src/components/CalendarDay.tsx
@@ -1,52 +1,41 @@
+import { useState } from 'react'
 import { daysIntoYear } from '../functions/functions'
 
 export default function CalendarDay({
-	display,
+	inDisplay,
 	isWeekend,
 	isPublicHoliday,
 	isBestDay,
 	index
 }: {
-	display: number
+	inDisplay: number
 	isWeekend?: boolean
 	isPublicHoliday?: boolean
 	isBestDay: boolean
 	index: number
 }) {
-	if (index < daysIntoYear(new Date())) {
-		return (
-			<p className="text-center text-copy-lighter" key={index}>
-				{display}
-			</p>
-		)
-	}
+	const [display, setDisplay] = useState<number>(inDisplay)
 
-	if (isPublicHoliday) {
-		return (
-			<p className="text-center text-primary" key={index}>
-				{display}
-			</p>
-		)
-	}
-
-	if (isWeekend) {
-		return (
-			<p className="text-center text-error" key={index}>
-				{display}
-			</p>
-		)
-	}
-
-	if (isBestDay) {
-		return (
-			<p className="text-center text-secondary" key={index}>
-				{display}
-			</p>
-		)
+	const textColor = () => {
+		if (index < daysIntoYear(new Date())) return 'text-copy-lighter'
+		if (isPublicHoliday) return 'text-primary'
+		if (isWeekend) return 'text-error'
+		if (isBestDay) return 'text-secondary'
+		// if (index >= 79 && index <= 83) return 'text-emerald-500' // 10 days in march, 4 pto
+		// if (index >= 86 && index <= 90) return 'text-emerald-500' // 8 days in may, 3 pto
+		// if (index >= 120 && index <= 126) return 'text-emerald-500' // 9 days in march, 4 pto
+		// if (index >= 358 && index <= 362) return 'text-emerald-500' // 9 days in december, 3 pto
+		return 'text-copy'
 	}
 
 	return (
-		<p className="text-center text-copy" key={index}>
+		<p
+			className={`text-center ${textColor()}`}
+			key={index}
+			title={index.toString()}
+			onMouseOver={() => setDisplay(index)}
+			onMouseOut={() => setDisplay(inDisplay)}
+		>
 			{display}
 		</p>
 	)
