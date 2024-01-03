72c884a10ba93a359e06842257c89a3f9b8df2e6
himan7991 <himan7991@gmail.com>
2024-01-03 15:53:32 +0200

    feat: set the country by the user's IP

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import Content from './components/Content'
 import { useContext, useEffect, useState } from 'react'
 import AppContext from './context/AppContext'
 import { getDaysInYear, getPublicHolidays, getWeekends } from './functions/functions'
+import { IpApi } from './types/IpApi'
 
 export default function App() {
 	// context
 	const [year, setYear] = useState<number>(new Date().getFullYear())
-	const [country, setCountry] = useState<string>('GR')
+	const [country, setCountry] = useState<string>('US')
 	const [daysInYear, setDaysInYear] = useState<number>(365)
 	const [weekends, setWeekends] = useState<number[]>([])
 	const [publicHolidays, setPublicHolidays] = useState<number[]>([])
+	const [userIP, setUserIP] = useState<string>('')
 
 	const passedAppContext = {
 		year,
@@ -27,17 +29,23 @@ export default function App() {
 	}
 
 	useEffect(() => {
-		const fetchPublicHolidays = async () => {
-			try {
-				await fetch(`https://date.nager.at/api/v3/PublicHolidays/${year}/${country}`)
-					.then((response) => response.json())
-					.then((data) => setPublicHolidays(getPublicHolidays(data)))
-			} catch (error) {
-				console.error('Error fetching data:', error)
-			}
-		}
-
-		fetchPublicHolidays()
+		// get the user's ip
+		fetch('https://api.ipify.org?format=json')
+			.then((ipResponse) => ipResponse.json())
+			.then((ipData) => {
+				setUserIP(ipData.ip)
+				// use the ip to retrieve the user's location
+				return fetch(`https://ipapi.co/${ipData.ip}/json/`)
+			})
+			.then((countryResponse) => countryResponse.json())
+			.then((countryData) => {
+				setCountry(countryData.country_code)
+				// use the user country to retrieve the holidays in that country
+				return fetch(`https://date.nager.at/api/v3/PublicHolidays/${year}/${countryData.country_code}`)
+			})
+			.then((holidaysResponse) => holidaysResponse.json())
+			.then((holidaysData) => setPublicHolidays(getPublicHolidays(holidaysData)))
+			.catch((error) => console.error('Error fetching data:', error))
 
 		setDaysInYear(getDaysInYear(year))
 		setWeekends(getWeekends(year))
---
+++ src/types/IpApi.ts
@@ -0,0 +1,24 @@
+export type IpApi = {
+	ip: string
+	city: string
+	region: string
+	region_code: string
+	country_code: string
+	country_code_iso3: string
+	country_name: string
+	country_capital: string
+	country_tld: string
+	continent_code: string
+	in_eu: boolean
+	postal: string
+	latitude: number
+	longitude: number
+	timezone: string
+	utc_offset: string
+	country_calling_code: string
+	currency: string
+	currency_name: string
+	languages: string
+	asn: string
+	org: string
+}
