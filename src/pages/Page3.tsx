ad8a75ba7f1d4bea87122040d4a1f6c8413dca89
himan7991 <himan7991@gmail.com>
2024-01-03 23:29:14 +0200

    feat(Page3): should now actually find the best time to take vacation
    It should now, theoretically, find the best days to take off, given an amount of set days of PTO

--- src/pages/Page3.tsx
+++ src/pages/Page3.tsx
@@ -2,22 +2,68 @@ import { motion } from 'framer-motion'
 import { useContext } from 'react'
 import CalendarDay from '../components/CalendarDay'
 import AppContext from '../context/AppContext'
+import { daysIntoYear } from '../functions/functions'
+import { supportedCountries } from '../constants/SupportedCountries'
 
 export default function Page3() {
 	const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 	const weekdays = ['M', 'T', 'W', 'T', 'F', 'S', 'S']
 	let count = 0
 
-	const { daysInYear, weekends, publicHolidays, year } = useContext(AppContext)
+	const { countryCode, daysInYear, weekends, publicHolidays, year } = useContext(AppContext)
+	const daysInYearArr: number[] = Array.from({ length: daysInYear }, (_, index) => index + 1)
+	const workingDays: number[] = daysInYearArr.filter(
+		(day) => !weekends.includes(day) && !publicHolidays.includes(day) && day >= daysIntoYear(new Date())
+	)
+
+	const getBestDaysToTakeOff = (workingDays: number[], maxDays: number): number[] => {
+		const sortedWorkingDays = [...workingDays].sort((a, b) => a - b)
+
+		let bestDays: number[] = []
+		let currentVacation: number[] = []
+
+		for (let i = 0; i < sortedWorkingDays.length; i++) {
+			const currentDay = sortedWorkingDays[i]
+
+			const isNextToPublicHoliday = publicHolidays.includes(currentDay - 1) || publicHolidays.includes(currentDay + 1)
+
+			const includesWeekend = weekends.includes(currentDay - 1) || weekends.includes(currentDay + 1)
+
+			if (isNextToPublicHoliday || includesWeekend) {
+				currentVacation.push(currentDay)
+			} else {
+				if (currentVacation.length > bestDays.length) {
+					bestDays = [...currentVacation]
+				}
+				currentVacation = []
+			}
+		}
+
+		return bestDays.slice(0, maxDays)
+	}
+
+	// Specify the maximum number of days you want to take off
+	const maxVacationDays = 4 // Adjust as needed
+
+	const bestDays: number[] = getBestDaysToTakeOff(workingDays, maxVacationDays)
+	console.log('🚀 ~ file: Page3.tsx:52 ~ Page3 ~ bestDays:', bestDays)
 
 	return (
-		<motion.div id="page3" className="w-full p-4 flex flex-col justify-around items-center h-full xl:flex-row" layoutId="pages">
-			<div className="flex flex-col items-center mb-4 xl:mb-0">
-				<h2 className="text-6xl text-center font-bold text-copy mb-4">Overview</h2>
-				<p className="max-w-[40ch] text-center text-copy">
-					Days in <span className="text-primary font-semibold">purple</span> are the public holidays in {year}, while days in{' '}
-					<span className="text-error font-semibold">red</span> are the weekends.
-				</p>
+		<motion.div id="page3" className="w-full p-4 flex flex-col justify-center items-center h-full xl:flex-row" layoutId="pages">
+			<div className="flex items-center justify-center relative w-full">
+				<div className="flex flex-col items-center mb-4 xl:mb-0">
+					<h2 className="text-6xl text-center font-bold text-copy mb-4">Overview</h2>
+					{/* <p className="max-w-[40ch] text-center text-copy">
+						Days in <span className="text-primary font-semibold">purple</span> are the public holidays in {year}, while days in{' '}
+						<span className="text-error font-semibold">red</span> are the weekends.
+					</p> */}
+					<p className="text-copy text-center">
+						We found <span className="font-semibold text-primary">{publicHolidays.length} public holidays</span> in{' '}
+						{supportedCountries.filter((c) => c.countryCode === countryCode)[0].name},{' '}
+						<span className="font-semibold text-error">{Math.floor(weekends.length / 2)} weekends</span> and <br />
+						<span className="font-semibold">{workingDays.length} working days ahead</span>
+					</p>
+				</div>
 			</div>
 			<div className="grid grid-cols-4 gap-4">
 				{months.map((month, m) => {
@@ -43,14 +89,15 @@ export default function Page3() {
 								<p key={`empty-${index}`} />
 							))}
 
-							{[...Array(daysInYear)]
+							{daysInYearArr
 								.map((_, j) => {
 									return (
 										<CalendarDay
 											index={j + 1}
-											display={j + 1 + daysInMonth - count}
+											inDisplay={j + 1 + daysInMonth - count}
 											isWeekend={weekends.includes(j + 1)}
 											isPublicHoliday={publicHolidays.includes(j + 1)}
+											isBestDay={bestDays.includes(j + 1)}
 											key={j}
 										/>
 									)
