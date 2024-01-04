239c622dd8459a6a4e08d8097015eb9c38510961
himan7991 <himan7991@gmail.com>
2024-01-04 23:30:57 +0200

    fix(getBestDays): now ACTUALLY returns the correct best days and the amount

--- src/functions/getBestDays.ts
+++ src/functions/getBestDays.ts
@@ -1,39 +1,55 @@
 import { BestDay } from '../types/BestDays'
 
 export const getBestDaysToTakeOff = (workingDays: number[], maxDays: number, weekends: number[], publicHolidays: number[]): BestDay[] => {
-	let currentVacation: { day: number; points: number }[] = []
+	let bestDays: BestDay[] = []
+	let workingDaysWithPoints: { day: number; pointDays: number[] }[] = []
 
 	const isWeekendOrHoliday = (day: number): boolean => weekends.includes(day) || publicHolidays.includes(day)
 
-	const calculatePoints = (day: number): number => {
-		let points = 0
+	const createPointDays = (day: number): number[] => {
+		const pointDays: number[] = []
 
-		// Check days before
 		let consecutiveDaysBefore = 1
 		while (isWeekendOrHoliday(day - consecutiveDaysBefore)) {
-			points += 1
+			pointDays.push(day - consecutiveDaysBefore)
 			consecutiveDaysBefore++
 		}
 
-		// Check days after
 		let consecutiveDaysAfter = 1
 		while (isWeekendOrHoliday(day + consecutiveDaysAfter)) {
-			points += 1
+			pointDays.push(day + consecutiveDaysAfter)
 			consecutiveDaysAfter++
 		}
 
-		return points
+		return pointDays
 	}
 
 	for (let i = 0; i < workingDays.length; i++) {
 		const currentDay = workingDays[i]
-		const currentPoints = calculatePoints(currentDay)
+		const currentPointDays = createPointDays(currentDay)
 
-		if (currentPoints > 0) {
-			currentVacation.push({ day: currentDay, points: currentPoints })
+		if (currentPointDays.length > 0) {
+			workingDaysWithPoints.push({ day: currentDay, pointDays: currentPointDays })
 		}
 	}
 
-	currentVacation = currentVacation.sort((a, b) => b.points - a.points).slice(0, maxDays)
-	return currentVacation
+	workingDaysWithPoints
+		.sort((a, b) => b.pointDays.length - a.pointDays.length)
+		.forEach((obj) => {
+			const currentDay = obj.day
+			const otherObjects = workingDaysWithPoints.filter((o) => o.day !== currentDay)
+			obj.pointDays.forEach((pointDay) => {
+				otherObjects.forEach((otherObj) => {
+					const index = otherObj.pointDays.indexOf(pointDay)
+					if (index !== -1) otherObj.pointDays.splice(index, 1)
+				})
+			})
+		})
+
+	bestDays = workingDaysWithPoints
+		.filter((obj) => obj.pointDays.length > 0)
+		.slice(0, maxDays)
+		.map((obj) => ({ day: obj.day, points: obj.pointDays.length }))
+
+	return bestDays
 }
